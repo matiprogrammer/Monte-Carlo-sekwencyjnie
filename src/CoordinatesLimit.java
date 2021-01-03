80daa46e6835f80dadc801b147eb5d062e649a7c
Mateusz <nawrockimateusz96@gmail.com>
2021-01-03 12:44:06 +0100

    one more class

---
+++ src/CoordinatesLimit.java
@@ -0,0 +1,16 @@
+public class CoordinatesLimit {
+    private double upperLimit, lowerLimit;
+
+    public CoordinatesLimit( double lowerLimit,double upperLimit) {
+        this.upperLimit = upperLimit;
+        this.lowerLimit = lowerLimit;
+    }
+
+    public double getUpperLimit() {
+        return upperLimit;
+    }
+
+    public double getLowerLimit() {
+        return lowerLimit;
+    }
+}

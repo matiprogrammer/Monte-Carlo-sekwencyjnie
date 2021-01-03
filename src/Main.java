de109d04ccdb8048ca458df8a11e9c5d14d83e65
Mateusz <nawrockimateusz96@gmail.com>
2021-01-03 18:08:50 +0100

    time measurement

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,13 @@
 import java.util.Random;
 
 public class Main {
-
-    /*x1≥0,x2≥0,x3≥0,
-    x1^2+x2^2+x3^2≤12,
-    x1^2+x3^2≤7
-     */
     public static void main(String[] args) {
-        MonteCarlo monteCarlo = new MonteCarlo(1000000);
-        double solidVolume = monteCarlo.compute();
-        System.out.println(solidVolume);
+        MonteCarlo monteCarlo = new MonteCarlo(123456789);
+        long startTime=System.nanoTime();
+        double solidVolume= monteCarlo.compute();
+        long endTime=System.nanoTime();
+        System.out.println("Objetosc: "+solidVolume);
+        System.out.println("czas wykoniania: "+(endTime-startTime)/1000000+"ms");
     }
 
 }
